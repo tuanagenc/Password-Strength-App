a5b7ab761dc1781b94f608c0208b185d4b7a4732
tuanagenc028@gmail.com <tuanagenc028@gmail.com>
2024-03-19 14:52:29 +0300

    Color transitions added

--- app.js
+++ app.js
@@ -12,11 +12,19 @@ pass.addEventListener('input', () => {
     //zayıf şifre yazısı
     if (pass.value.length < 4) {
         str.innerHTML = "weak";
+        //renk degişimi
+        pass.style.borderColor = "#ff5925";
+        msg.style.color = "#ff5925";
+
     }
     else if (pass.value.length >= 4 && pass.value.length < 8) {
         str.innerHTML = "medium";
+        pass.style.borderColor = "yellow";
+        msg.style.color = "yellow";
     }
     else if (pass.value.length >= 8) {
         str.innerHTML = "strong";
+        pass.style.borderColor = "#26d730";
+        msg.style.color = "#26d730";
     }
 })
