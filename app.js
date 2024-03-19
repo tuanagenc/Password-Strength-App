1f0e8cc8460a21954d74a33c84fbb5a971358b49
tuanagenc028@gmail.com <tuanagenc028@gmail.com>
2024-03-19 14:40:43 +0300

    Password length added

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ var msg = document.getElementById("message");
 var str = document.getElementById("strenght");
 
 pass.addEventListener('input', () => {
-    if (pass.ariaValueMax.length > 0) {
+    if (pass.value.length > 0) {
         msg.style.display = "block";
     }
-    else{
+    else {
         msg.style.display = "none";
+    }
+    //zayıf şifre yazısı
+    if (pass.value.length < 4) {
+        str.innerHTML = "weak";
+    }
+    else if (pass.value.length >= 4 && pass.value.length < 8) {
+        str.innerHTML = "medium";
+    }
+    else if (pass.value.length >= 8) {
+        str.innerHTML = "strong";
     }
 })
